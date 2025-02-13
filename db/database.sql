409adbdcb733ca7323ae7364f24d797b334104eb
arindam <arindammannawork@gmail.com>
2025-02-13 22:58:06 +0530

    db

--- db/database.sql
+++ db/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 08, 2023 at 02:03 AM
--- Server version: 5.7.42-cll-lve
--- PHP Version: 7.4.33
+-- Host: localhost
+-- Generation Time: Feb 13, 2025 at 05:17 AM
+-- Server version: 11.4.4-MariaDB
+-- PHP Version: 8.3.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Social Hoodcu_ma`
+-- Database: `socialhood`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `username` varchar(255) NOT NULL,
+  `friend_username` varchar(255) NOT NULL,
+  `status` enum('pending','accepted','declined') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`username`, `friend_username`, `status`) VALUES
+('souvik', 'arindam', 'pending');
 
 -- --------------------------------------------------------
 
@@ -33,7 +52,7 @@ CREATE TABLE `messages` (
   `message` mediumtext NOT NULL,
   `room_id` int(11) NOT NULL,
   `dos` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,8 +66,17 @@ CREATE TABLE `posts` (
   `msg` mediumtext NOT NULL,
   `image` varchar(50) DEFAULT NULL,
   `type` varchar(1) NOT NULL DEFAULT 'p',
-  `dop` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `dop` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`pid`, `uid`, `msg`, `image`, `type`, `dop`) VALUES
+(1, 2, 'hello world\r\n', NULL, 'p', '2025-02-10 12:02:34'),
+(2, 1, 'hey friends', '67aa089ff1004.jpg', 'p', '2025-02-10 14:09:35'),
+(3, 1, 'sss', '67ab423984025.png', 'p', '2025-02-11 12:27:37');
 
 -- --------------------------------------------------------
 
@@ -60,7 +88,7 @@ CREATE TABLE `rooms` (
   `room_id` int(11) NOT NULL,
   `recp1` int(11) NOT NULL,
   `recp2` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -70,20 +98,23 @@ CREATE TABLE `rooms` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL UNIQUE,
+  `username` varchar(20) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
-  `email` varchar(256) NOT NULL UNIQUE,
+  `email` varchar(256) NOT NULL,
   `password` text NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` int(11) NOT NULL DEFAULT 0,
+  `profile_pic` varchar(50) DEFAULT NULL,
+  `cover_pic` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `friends` (
-  `id` int(11) NOT NULL,
-  `from` varchar(20) NOT NULL UNIQUE,
-  `to` varchar(50) NOT NULL,
-  `isAccepected` bool(50) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `fname`, `lname`, `email`, `password`, `status`, `profile_pic`, `cover_pic`) VALUES
+(1, 'arindam', 'Arindam', 'Manna', 'arindammannawork@gmail.com', '$1$$iC.dUsGpxNNJGeOm1dFio/', 0, '67ab3f22be403.', '67ab3f22be434.'),
+(2, 'souvik', 'souvik', 'samanta', 'souvik@gmail.com', '$1$$iC.dUsGpxNNJGeOm1dFio/', 0, '67ad7fd97a8dd.jpg', '67ad7fd97c75a.png');
 
 --
 -- Indexes for dumped tables
@@ -113,7 +144,9 @@ ALTER TABLE `rooms`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -129,7 +162,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `rooms`
@@ -141,7 +174,7 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
